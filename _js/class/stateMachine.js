1f2a63dbc5432ca9cec8a3b050dfc7ef77aec0c4
YI ZHANG <scaccterran@gmail.com>
2018-03-12 11:26:26 -0400

    stack based finite state machine

---
+++ _js/class/stateMachine.js
@@ -0,0 +1,59 @@
+//stack based finite state machine
+export default class StateMachine {
+
+    constructor(originator, defaultState) {
+
+        this.originator = originator;
+        this.states = [];
+        this.defaultState = defaultState;
+        //initialize state tracker if default state is given
+        if(defaultState) {
+
+            this.push(defaultState);
+        }
+    }
+
+    //current active state
+    get active() {
+
+        return this.states.length ? this.states.slice(-1)[0] : null;
+    }
+
+    reset() {
+
+        this.states = this.defaultState ? [this.defaultState] : [];
+    }
+
+    push(state) {
+
+        if(this.active !== state) {
+
+            this.states.push(state);
+        }
+    }
+
+    //remove current active state
+    pop() {
+
+        this.states.pop();
+    }
+
+    //swap current active state with given state
+    swap(state) {
+
+        if(this.active) {
+
+            this.pop();
+        }
+
+        this.push(state);
+    }
+
+    update(timeStep) {
+
+        if(this.active) {
+
+            this.originator[this.active](timeStep);
+        }
+    }
+}
