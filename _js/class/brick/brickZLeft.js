4bfd1fd8f1af3edbc8c92c8128ed869e1e6085c4
YI ZHANG <scaccterran@gmail.com>
2018-03-14 03:22:11 -0400

    Z-shape bricks

---
+++ _js/class/brick/brickZLeft.js
@@ -0,0 +1,49 @@
+import Brick from "_js/class/brick/brick";
+
+export default class BrickZLeft extends Brick {
+
+    constructor(originator, color, orientation = "up") {
+
+        super(originator, color, orientation);
+        //patterns
+        this.up = [
+
+            [0, 0, 1, 0],
+            [0, 1, 1, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0]
+        ];
+
+        this.left = [
+
+            [0, 0, 0, 0],
+            [1, 1, 0, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ];
+
+        this.down = this.up;
+        this.right = this.left;
+        //icons
+        this.upIcon = [
+
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0],
+            [0, 0, 1, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0]
+        ];
+
+        this.leftIcon = [
+
+            [0, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0],
+            [0, 0, 1, 1, 0],
+            [0, 0, 0, 0, 0]
+        ];
+
+        this.downIcon = this.upIcon;
+        this.rightIcon = this.leftIcon;
+        this.blocks = this[orientation];
+    }
+}
